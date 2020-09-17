1514d215cf11f7ec082eb63a6c1983e8b91eabba
damien.de-sousa@etu.umontpellier.fr <desousadamien30@gmail.com>
2020-09-17 15:22:37 +0200

    add comments

--- app/src/Twig/Admin/Extension.php
+++ app/src/Twig/Admin/Extension.php
@@ -1,12 +1,25 @@
 <?php
 
+/**
+ * Define the Twig admin extensions.
+ *
+ * @author    Damien DE SOUSA <email@email.com>
+ * @copyright 2020 Damien DE SOUSA
+ */
+
 namespace App\Twig\Admin;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
+/**
+ * Declare admin Twig extensions.
+ */
 class Extension extends AbstractExtension
 {
+    /**
+     * @inheritDoc
+     */
     public function getFunctions()
     {
         return [
