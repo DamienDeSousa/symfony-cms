9da6e98fe11354ac4e18737b25b35fccca94fa5c
Damien DE SOUSA <damien.desousa@smile.fr>
2021-10-27 17:22:52 +0200

    #create block type grid action
     * add route
     * add test
     * add page

---
+++ app/src/Tests/Provider/Data/BlockTypeProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * File that defines the BlockTypeProvider trait.
+ *
+ * @author Damien DE SOUSA
+ * @copyright 2021
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Provider\Data;
+
+use App\Entity\Structure\BlockType;
+
+/**
+ * Trait used to provide block type entity for test.
+ */
+trait BlockTypeProvider
+{
+    public function provideBlockType(): BlockType
+    {
+        $blockType = new BlockType();
+        $blockType->setType('header');
+
+        return $blockType;
+    }
+}
