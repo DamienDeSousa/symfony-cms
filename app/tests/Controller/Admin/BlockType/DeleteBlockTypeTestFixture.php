ecd7fcb7df8726dbcd2ae877f8c928698b68578a
DamienDeSousa <desousadamien30@gmail.com>
2021-11-06 15:34:30 +0100

    #delete_block_type

---
+++ app/tests/Controller/Admin/BlockType/DeleteBlockTypeTestFixture.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * File that defines the DeleteBlockTypeTestFixture class.
+ *
+ * @author    Damien DE SOUSA
+ * @copyright 2021
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller\Admin\BlockType;
+
+use Doctrine\Persistence\ObjectManager;
+use App\Tests\Provider\Data\UserProvider;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Tests\Provider\Data\BlockTypeProvider;
+
+/**
+ * This class is used to load fixtures for the DeleteBlockTypeTest class.
+ */
+class DeleteBlockTypeTestFixture extends Fixture
+{
+    use BlockTypeProvider;
+
+    use UserProvider;
+
+    public function load(ObjectManager $manager)
+    {
+        $blockType = $this->provideBlockType();
+        $manager->persist($blockType);
+        $user = $this->provideSuperAdminUser();
+        $manager->persist($user);
+        $manager->flush();
+        $this->referenceRepository->addReference('user', $user);
+        $this->referenceRepository->addReference('block_type', $blockType);
+    }
+}
